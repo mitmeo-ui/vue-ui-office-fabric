4dc4dad691405b883a0c844dbeb0cf6b6e8471f9
Hugh Hoang <hugh.hoang@gmail.com>
2017-06-10 22:58:06 +1200

    Move button logic to a mixin so icon-button can reused it.

---
+++ src/components/mixins/button.js
@@ -0,0 +1,55 @@
+import utils from '@/utils';
+
+export default {
+  props: [
+    'type',
+    'loading',
+    'loading-text',
+  ],
+  data() {
+    return {
+      cssClass: 'ms-Button',
+      disabled: false,
+    };
+  },
+  mounted() {
+    // First undefined tag is the button text
+    this.textSlot = utils.getSlotNodesByTag(this)[0];
+    this.defaultText = this.textSlot.text;
+    this.setText = (text) => {
+      if (this.textSlot.elm) this.textSlot.elm.data = text;
+      else this.textSlot.text = text;
+    };
+    this.checkLoading = (val) => {
+      if (val) {
+        this.disabled = true;
+        this.setText(this.computedLoadingText);
+      } else {
+        this.disabled = false;
+        this.setText(this.defaultText);
+      }
+    };
+    this.checkType = (val) => {
+      if (val && val !== 'default') {
+        this.cssClass += ` ms-Button--${this.type}`;
+      } else {
+        this.cssClass = 'ms-Button';
+      }
+    };
+    this.checkLoading(this.loading);
+    this.checkType(this.type);
+  },
+  watch: {
+    loading(val) {
+      this.checkLoading(val);
+    },
+    type(val) {
+      this.checkType(val);
+    },
+  },
+  computed: {
+    computedLoadingText() {
+      return this.loadingText || 'Loading...';
+    },
+  },
+};
